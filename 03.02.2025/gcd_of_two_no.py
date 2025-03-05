6ad697817a08663ef6a27c6ff081a4a57eb0d6e9
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-05 11:05:48 +0530

    Fixed GCD function and added files

--- 03.02.2025/gcd_of_two_no.py
+++ 03.02.2025/gcd_of_two_no.py
@@ -1,15 +1,16 @@
-def gcd (num1 : int,num2 : int) -> int :
-    if(num1 ==0 or num2== 0) :
+def gcd(num1: int, num2: int) -> int:
+    if num1 == 0 or num2 == 0:
         return 0
-    elif (num1 == num2) :
+    elif num1 == num2:
         return num1
-    else :
-        for i in range(1 , max(num1 , num2) +1 ) :
-            if num1 %i ==0 and num2 %i ==0 :
-                gcd_value =i
-        return gcd_value;
-        
+    else:
+        gcd_value = 1
+        for i in range(1, min(num1, num2) + 1):
+            if num1 % i == 0 and num2 % i == 0:
+                gcd_value = i
+        return gcd_value
+
 a = int(input("Please enter number1: "))
 b = int(input("Please enter number2: "))
-gcd_value =1
+gcd_value = 1
 print("The GCD of the numbers is:", gcd(a, b))
