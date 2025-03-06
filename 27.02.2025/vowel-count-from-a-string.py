ea4a9e854b1e19a659d424b3a7484fd0ff4ce234
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-06 07:42:19 +0530

    Fix vowel count script to use __len__ method for string length

--- 27.02.2025/vowel-count-from-a-string.py
+++ 27.02.2025/vowel-count-from-a-string.py
@@ -1,6 +1,7 @@
 print("Enter a string: ")
 string = input()
-l = string.length()
+l = string.__len__()
 print("the vowels in the string are: ")
 for i in range (0, l):
-    
+    if(string[i] == 'a' or string[i] == 'e' or string[i] == 'i' or string[i] == 'o' or string[i] == 'u'):
+        print(string[i])
