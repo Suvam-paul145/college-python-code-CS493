77012d3103c48ff9a772b3b125b8b487e2adfe3a
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-06 07:40:09 +0530

    Add multiplication table and vowel count scripts

---
+++ 27.02.2025/multiplicaton-table.py
@@ -0,0 +1,6 @@
+print("The multiplication table of 1 to 20 is here: ")
+for i in range (1,21):
+    print("The table of ", i, " ") 
+    for j in range(1, 11):
+        print(i, "x", j, "=", i*j)
+
---
+++ 27.02.2025/vowel-count-from-a-string.py
@@ -0,0 +1,6 @@
+print("Enter a string: ")
+string = input()
+l = string.length()
+print("the vowels in the string are: ")
+for i in range (0, l):
+    
