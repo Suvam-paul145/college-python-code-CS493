d0aff7555a535d326a377bb29c81e2076577e6fb
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-07 07:49:42 +0530

    Implement binary search algorithm with test cases

--- 27.02.2025/binary-search.py
+++ 27.02.2025/binary-search.py
@@ -1,3 +1,34 @@
-arr = [1,5,7,2,9,9,95,5,434,6]
-new = sorted(arr)
-print(new)
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        # If x is greater, ignore left half
+        if arr[mid] < x:
+            low = mid + 1
+
+        # If x is smaller, ignore right half
+        elif arr[mid] > x:
+            high = mid - 1
+
+        # x is present at mid
+        else:
+            return mid
+
+    # x is not present in array
+    return -1
+
+# Test the binary search function
+arr = [2, 3, 4, 10, 40]
+x = 10
+
+# Function call
+result = binary_search(arr, x)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
