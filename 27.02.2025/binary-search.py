205b6f1951b6796c2a6e95569bd2709685d5c2f4
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-07 07:39:00 +0530

    Add binary search implementation and element count script for a list

---
+++ 27.02.2025/binary-search.py
@@ -0,0 +1,3 @@
+arr = [1,5,7,2,9,9,95,5,434,6]
+new = sorted(arr)
+print(new)
---
+++ 27.02.2025/tuples/element-count.py
@@ -0,0 +1,4 @@
+list = ["suvam", '123', 12, '123',123,"Suvam"]
+for i in range(0,len(list) ):
+    print(list[i]," ", type(list[i]), "occurances in the list is: ", list.count(list[i]))
+    
