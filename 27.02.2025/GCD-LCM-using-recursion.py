badefd99fbfa57ff17b60aa6044341d4aa95a70d
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-07 07:03:45 +0530

    Refactor GCD function to handle cases with a and b being multiples, and add user input for GCD and LCM calculation

--- 27.02.2025/GCD-LCM-using-recursion.py
+++ 27.02.2025/GCD-LCM-using-recursion.py
@@ -1,6 +1,22 @@
 def gcd(a,b):
     if a == b :
         return a
-    elif
+    else:
         if a>=2*b:
-            
+            for i in range(2,b):
+                if(a % i ==0 & b % i ==0):
+                    gcd = i
+            return gcd
+        else:
+            if b>=2*a:
+                for i in range(2,a):
+                    if(a % i ==0 & b % i ==0):
+                        gcd = i
+                return gcd
+
+print("Enter two no for finding LCM and GCD ")
+a = int(input("Enter first no: "))
+b = int(input("Enter second no: "))
+print("The GCD is: ", gcd(a,b))
+print("The LCM is:", a*b/gcd(a,b)) 
+        
